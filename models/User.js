6cc00488a873e1b1ec7e0847e8b32f78191a2304
Tihomir Bogajcevic <tihomirbogajcevic94@hotmail.com>
2020-06-01 12:51:32 +0200

    User model, validation and password hashing

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const moongose = require('mongoose');
+
+const UserSchema = moongose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = moongose.model('user', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
 
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
-router.post('/', (req, res) => {
-    res.send('Register a user');
-});
+router.post(
+    '/',
+    [
+        // express-validator
+        check('name', 'Please add name').not().isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({
+            min: 6,
+        }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+            if (user) {
+                return res.status(400).json({ msg: 'User already exists' });
+            }
+
+            user = new User({ name, email, password });
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            res.send('User saved');
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+);
 
 // We have to export the router in order for this to work
 module.exports = router;

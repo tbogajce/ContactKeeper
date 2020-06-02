412f85897c22356d1d360ac034c1139f1eb8b97e
Tihomir Bogajcevic <tihomirbogajcevic94@hotmail.com>
2020-06-02 13:44:01 +0200

    Implemented editing Contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,6 +37,7 @@ const ContactState = (props) => {
                 type: 'professional',
             },
         ],
+        current: null,
     };
 
     const [state, dispatch] = useReducer(ContactReducer, initialState);
@@ -53,10 +54,19 @@ const ContactState = (props) => {
     };
 
     // Set Current Contact
+    const setCurrent = (contact) => {
+        dispatch({ type: SET_CURRENT, payload: contact });
+    };
 
     // Clear Current Contact
+    const clearCurrent = () => {
+        dispatch({ type: CLEAR_CURRENT });
+    };
 
     // Update Contact
+    const updateContact = (contact) => {
+        dispatch({ type: UPDATE_CONTACT, payload: contact });
+    };
 
     // Filter Contacts
 
@@ -66,8 +76,12 @@ const ContactState = (props) => {
         <ContactContext.Provider
             value={{
                 contacts: state.contacts,
+                current: state.current,
                 addContact,
                 deleteContact,
+                setCurrent,
+                clearCurrent,
+                updateContact,
             }}
         >
             {props.children}
